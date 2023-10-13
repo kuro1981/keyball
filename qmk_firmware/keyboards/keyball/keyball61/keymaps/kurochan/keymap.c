b524511fcc32d2fa9149bbe3b92ff78824fba58a
akaikaze <akaikaze.monokuro@gmail.com>
2023-10-13 13:23:30 +0900

    change keymap

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/kurochan/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/kurochan/keymap.c
@@ -31,30 +31,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOTE ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_BSLS  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-    KC_LCTL  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),MT_SS    ,LT(3,KC_LNG1),           KC_BSPC,LT(2,KC_ENT)                               , KC_RALT  , A(KC_SPC)
+    KC_LCTL  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(1,KC_LNG2),MT_SS    ,LT(3,KC_LNG1),           KC_BSPC,LT(2,KC_ENT)                               , KC_RALT  , RALT(KC_SPC)
   ),
 
   [1] = LAYOUT(
-    _______  , S(KC_1)  , S(KC_2)  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  KC_EQL   , S(KC_6)  ,S(KC_QUOT), S(KC_8)  , S(KC_9)  ,S(KC_INT1),
-    S(KC_DEL), S(KC_Q)  , S(KC_W)  , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)  ,S(KC_INT3),
-    S(KC_TAB), S(KC_A)  , S(KC_S)  , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  , KC_QUOT  , S(KC_2)  ,
+    _______  , S(KC_1)  , S(KC_2)  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                  S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  ,S(KC_EQUAL),
+    S(KC_DEL), S(KC_Q)  , S(KC_W)  , S(KC_E)  , S(KC_R)  , S(KC_T)  ,                                  S(KC_Y)  , S(KC_U)  , S(KC_I)  , S(KC_O)  , S(KC_P)  ,S(KC_MINS),
+    S(KC_TAB), S(KC_A)  , S(KC_S)  , S(KC_D)  , S(KC_F)  , S(KC_G)  ,                                  S(KC_H)  , S(KC_J)  , S(KC_K)  , S(KC_L)  ,S(KC_SCLN),S(KC_QUOTE)  ,
     _______  , S(KC_Z)  , S(KC_X)  , S(KC_C)  , S(KC_V)  , S(KC_B)  ,S(KC_BSLS),           S(KC_NUHS), S(KC_N)  , S(KC_M)  ,S(KC_COMM), S(KC_DOT),S(KC_SLSH),S(KC_RSFT),
     _______  ,S(KC_LCTL),S(KC_LALT),S(KC_LGUI), _______  , _______  , _______  ,            _______  , _______                                   , S(KC_RALT), KC_PSCR
   ),
 
   [2] = LAYOUT(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , DF(0)   ,
-    _______  , _______  , KC_7     , KC_8     , KC_9     , _______  ,                                  KC_PGUP  , KC_DOWN  , _______  , KC_F11   , KC_F12   , DF(2)   ,
-    _______  , _______  , KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______ ,
-    _______  , _______  , KC_1     , KC_2     , KC_3     ,S(KC_MINS), S(KC_8)  ,            S(KC_9)  , _______  , KC_BTN1  , KC_BTN1  , KC_BTN2  , KC_BTN3  , MO(3)   ,
+    _______  , _______  , KC_7     , KC_8     , KC_9     , XXXXXXX  ,                                  KC_PGUP  , KC_DOWN  , XXXXXXX  , KC_F11   , KC_F12   , DF(2)   ,
+    _______  , _______  , KC_4     , KC_5     , KC_6     , XXXXXXX  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , XXXXXXX  , XXXXXXX ,
+    _______  , _______  , KC_1     , KC_2     , KC_3     , KC_LBRC  , S(KC_9)  ,            S(KC_0)  , KC_RBRC  , KC_BTN1  , KC_BTN1  , KC_BTN2  , KC_BTN3  , SCRL_MO,
     _______  , _______  , KC_0     , KC_DOT   , _______  , _______  , _______  ,             KC_DEL  , _______                                   , _______  , KC_PSCR
   ),
 
   [3] = LAYOUT(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE  , KBC_RST  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , _______  ,                                  _______  , SCRL_DVI , SCRL_DVD , _______  , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
-    _______  , _______  , _______  , _______  , SCRL_MO  , SCRL_TO  , _______  ,            _______  , _______  , KC_BTN1  , KC_BTN1  , KC_BTN2  , KC_BTN3  , _______  ,
+    RGB_TOG  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KBC_RST  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , XXXXXXX  , XXXXXXX  ,                                  CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE , KBC_RST  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , XXXXXXX  , XXXXXXX  ,                                  XXXXXXX  , SCRL_DVI , SCRL_DVD , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , SCRL_MO  , SCRL_TO  , XXXXXXX  ,            XXXXXXX  , XXXXXXX  , KC_BTN1  , KC_BTN1  , KC_BTN2  , KC_BTN3  , XXXXXXX  ,
     _______  , _______  , KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  ,            _______  , KC_BSPC                                   , _______  , KC_PSCR
   ),
 };
